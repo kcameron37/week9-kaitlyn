26cdccf714c8f68c9e22734308ba404e652a9e42
kcameron37 <kcameron37@my.bcit.ca>
2023-03-02 14:07:07 -0800

    files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Class 9 Pop-up Kaitlyn Cameron
+## Made a popup that goes away when clicking close 
+## React Next.js
+## npm run dev / npx create-next-app week9  
---
+++ components/Ad/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import styles from './Ad.module.css'
+
+
+export default function Ad() {
+
+    const [popup, setPopup] = useState(true);
+        return (
+        <>
+             {
+                 popup ?
+                    <div className={styles.container}>
+                        <div className={styles.popup__area}>
+                             <div onClick={() => setPopup(false)}>Close</div>
+                             <h1> Welcome</h1>
+                        </div>
+                     </div>
+            :<> </>
+            }
+        </>
+    )
+}
